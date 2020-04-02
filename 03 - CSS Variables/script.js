1749fa02c2da0f6d6bf69ab6a38fcfce9ca8f6d5
popotanu <scrapmacaron@gmail.com>
2020-04-02 14:15:41 +0900

    done: 各スライダーバーのvalueに応じたstyleをあてる

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,1 +1,23 @@
-// spacing: 10-200
+"use strict";
+const setValue = elm => {
+  const propName = elm.id;
+  const sizing = elm.getAttribute("data-sizing") || "";
+  const value = elm.value;
+
+  valueStore.style.setProperty(`--${propName}`, `${value}${sizing}`);
+};
+
+const targetElements = ["#base", "#blur", "#spacing"];
+const valueStore = document.querySelector("body");
+
+window.addEventListener("load", () => {
+  targetElements.forEach(elm => {
+    setValue(document.querySelector(elm));
+  });
+});
+
+targetElements.forEach(elm => {
+  document.querySelector(elm).addEventListener("change", e => {
+    setValue(e.target);
+  });
+});

a7e6f2050c6f1161ecfb7d77cb396ff6ceed027a
popotanu <scrapmacaron@gmail.com>
2020-04-02 15:44:32 +0900

    minifix

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,23 +1,14 @@
 "use strict";
-const setValue = elm => {
-  const propName = elm.id;
-  const sizing = elm.getAttribute("data-sizing") || "";
-  const value = elm.value;
+const valueStore = document.documentElement;
 
-  valueStore.style.setProperty(`--${propName}`, `${value}${sizing}`);
+const updateEffect = elm => {
+  const sizing = elm.dataset.sizing || "";
+  valueStore.style.setProperty(`--${elm.name}`, `${elm.value}${sizing}`);
 };
 
-const targetElements = ["#base", "#blur", "#spacing"];
-const valueStore = document.querySelector("body");
+const inputs = document.querySelectorAll(".input");
 
-window.addEventListener("load", () => {
-  targetElements.forEach(elm => {
-    setValue(document.querySelector(elm));
-  });
-});
-
-targetElements.forEach(elm => {
-  document.querySelector(elm).addEventListener("change", e => {
-    setValue(e.target);
-  });
+inputs.forEach(input => window.addEventListener("load", updateEffect(input)));
+inputs.forEach(input => {
+  input.addEventListener("change", e => updateEffect(e.target));
 });
